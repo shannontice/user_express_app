ddc4453db4620778e81204f7eeca4acac0c86619
shannontice <stice14@gmail.com>
2024-01-24 12:54:01 -0500

    ready to deploy

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql2');
+require('dotenv').config();
+
+// IF you want to call process.env THEN you have to have the require config above it.
+
+const is_prod = process.env.NODE_ENV === 'production';
 
 
 // Create connection to our mysql database
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'mysql_first_day_db'
+    host: process.env.DB_HOST_URL,
+    user: process.env.DB_USER,
+    password: is_prod ? process.env.DB_PASSWORD : '',
+    database: process.env.DB_DATABASE_NAME
 });
 
 module.exports = db.promise();
